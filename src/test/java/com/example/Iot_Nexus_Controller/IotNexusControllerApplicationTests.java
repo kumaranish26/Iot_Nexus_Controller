643092ba385213a15b1fbf18663f65941cb30ca5
Anish Kumar <kumaranish2605@gmail.com>
2026-06-15 21:59:03 +0530

    Initial Commit

---
+++ src/test/java/com/example/Iot_Nexus_Controller/IotNexusControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Iot_Nexus_Controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotNexusControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
